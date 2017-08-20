c1361941da80360b03f31f69c430b3d67881ba47
Jack Schuss <jackschuss@gmail.com>
2017-08-20 10:38:15 -0400

    For Patients Page 2/4 sections

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -1,6 +1,14 @@
 $(function() {
   $(".inner-nav li").on("click", function(){
+    var oldId = $(".inner-nav").find(".active").attr("id")
+
     $(".inner-nav").find(".active").removeClass("active");
     $(this).addClass("active");
+
+    var id = $(this).attr("id")
+
+    $("." + id + '-section').removeClass("hidden");
+    $("." + oldId + '-section').addClass("hidden");
   });
+
 })
